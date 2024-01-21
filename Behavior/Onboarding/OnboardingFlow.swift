50c5e8ad2fe2bdd3356ef099e106bd3a3f4bed1f
dhruvna1k <dhruv.naik95@gmail.com>
2024-01-21 09:47:09 -0800

    Removing .task modifier for notifications Removing notifications view file since it is unused

--- Behavior/Onboarding/OnboardingFlow.swift
+++ Behavior/Onboarding/OnboardingFlow.swift
@@ -51,9 +51,6 @@ struct OnboardingFlow: View {
             }
             
         }
-            .task {
-                localNotificationAuthorization = await scheduler.localNotificationAuthorization
-            }
             .interactiveDismissDisabled(!completedOnboardingFlow)
     }
 }
