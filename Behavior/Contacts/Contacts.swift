2b317c817ff07d0891b54f49cc11ea18501ef657
Matthew Jörke <matthewjoerke@gmail.com>
2024-01-22 19:47:05 -0800

    Updated contacts

--- Behavior/Contacts/Contacts.swift
+++ Behavior/Contacts/Contacts.swift
@@ -16,35 +16,34 @@ struct Contacts: View {
     let contacts = [
         Contact(
             name: PersonNameComponents(
-                givenName: "Leland",
-                familyName: "Stanford"
+                givenName: "Matthew",
+                familyName: "Jörke"
             ),
-            image: Image(systemName: "figure.wave.circle"), // swiftlint:disable:this accessibility_label_for_image
-            title: "University Founder",
-            description: String(localized: "LELAND_STANFORD_BIO"),
-            organization: "Stanford University",
-            address: {
-                let address = CNMutablePostalAddress()
-                address.country = "USA"
-                address.state = "CA"
-                address.postalCode = "94305"
-                address.city = "Stanford"
-                address.street = "450 Serra Mall"
-                return address
-            }(),
+            title: "Study Coordinator & Developer",
             contactOptions: [
-                .call("+1 (650) 723-2300"),
-                .text("+1 (650) 723-2300"),
-                .email(addresses: ["contact@stanford.edu"]),
-                ContactOption(
-                    image: Image(systemName: "safari.fill"), // swiftlint:disable:this accessibility_label_for_image
-                    title: "Website",
-                    action: {
-                        if let url = URL(string: "https://stanford.edu") {
-                            UIApplication.shared.open(url)
-                        }
-                    }
-                )
+                .email(addresses: ["joerke@stanford.edu"]),
+            ]
+        ),
+        Contact(
+            name: PersonNameComponents(
+                namePrefix: "Prof.",
+                givenName: "James",
+                familyName: "Landay"
+            ),
+            title: "Principal Investigator",
+            contactOptions: [
+                .email(addresses: ["landay@stanford.edu"])
+            ]
+        ),
+        Contact(
+            name: PersonNameComponents(
+                namePrefix: "Prof.",
+                givenName: "Emma",
+                familyName: "Brunskill"
+            ),
+            title: "Principal Investigator",
+            contactOptions: [
+                .email(addresses: ["ebrun@stanford.edu"])
             ]
         )
     ]
