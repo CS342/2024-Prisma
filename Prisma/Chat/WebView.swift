666ea9729faa40cafa33538e10fd630dbbb1bfae
EvelynBunnyDev <evsong@stanford.edu>
2024-02-06 13:24:47 -0800

    Fixing implicit return violation

--- Prisma/Chat/WebView.swift
+++ Prisma/Chat/WebView.swift
@@ -13,7 +13,7 @@ struct WebView: UIViewRepresentable {
     var url: URL
 
     func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+        WKWebView()
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
