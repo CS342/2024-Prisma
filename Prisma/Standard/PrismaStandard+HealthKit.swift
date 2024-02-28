4e2794eff6d5f234d5870857e1e566fdff03903d
Matthew Jörke <matthewjoerke@gmail.com>
2024-02-28 15:19:50 -0800

    added time indexing fields to healthkit uploads;

--- Prisma/Standard/PrismaStandard+HealthKit.swift
+++ Prisma/Standard/PrismaStandard+HealthKit.swift
@@ -47,6 +47,114 @@ extension PrismaStandard {
         }
     }
     
+    func daysInMonth(month: Int, year: Int) -> Int {
+        let dateComponents = DateComponents(year: year, month: month)
+        let calendar = Calendar.current
+        let date = calendar.date(from: dateComponents)!
+        let range = calendar.range(of: .day, in: .month, for: date)!
+        return range.count
+    }
+    
+    // swiftlint:disable discouraged_optional_collection
+    func getRange(start: Any?, end: Any?, maxValue: Int, startValue: Int = 0) -> [Int]? {
+        guard let startInt = start as? Int, let endInt = end as? Int else {
+            return nil
+        }
+        
+        if startInt <= endInt {
+            return Array(startInt...endInt)
+        } else {
+            return Array(startInt...maxValue) + Array(startValue...endInt)
+        }
+    }
+    
+    // swiftlint:disable function_body_length
+    func getTimeIndex(sample: HKSample) -> [String: Any?] {
+        let calendar = Calendar.current
+        let startComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: sample.startDate)
+        let endComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: sample.endDate)
+        let isRange = sample.startDate != sample.endDate
+        
+        var timeIndex: [String: Any?] = [
+            "range": isRange,
+            "year.start": startComponents.year,
+            "month.start": startComponents.month,
+            "day.start": startComponents.day,
+            "hour.start": startComponents.hour,
+            "minute.start": startComponents.minute,
+            // number of total minutes elapsed in the day, between [0, 1439]
+            // flatMap defaults to nil if either .hour or .minute are nil
+            "dayMinute.start": startComponents.hour.flatMap { hour in
+                startComponents.minute.map { minute in
+                    hour * 60 + minute
+                }
+            },
+            // index of the 15min bucket that this sample falls into, between [0, 95]
+            "15minBucket.start": startComponents.hour.flatMap { hour in
+                startComponents.minute.map { minute in
+                    hour * 4 + minute / 15
+                }
+            },
+            "second.start": startComponents.second
+        ]
+        
+        if isRange { // only write end date and range if it is a range type
+            timeIndex["year.end"] = endComponents.year
+            timeIndex["month.end"] = endComponents.month
+            timeIndex["day.end"] = endComponents.day
+            timeIndex["hour.end"] = endComponents.hour
+            timeIndex["minute.end"] = endComponents.minute
+            timeIndex["dayMinute.end"] = endComponents.hour.flatMap { hour in
+                endComponents.minute.map { minute in
+                    hour * 60 + minute
+                }
+            }
+            timeIndex["15minBucket.end"] = endComponents.hour.flatMap { hour in
+                endComponents.minute.map { minute in
+                    hour * 4 + minute / 15
+                }
+            }
+            timeIndex["second.end"] = endComponents.second
+            
+            timeIndex["year.range"] = getRange(start: timeIndex["year.start"] as? Int, end: timeIndex["year.end"] as? Int, maxValue: Int.max)
+            timeIndex["month.range"] = getRange(
+                                            start: timeIndex["month.start"] as? Int,
+                                            end: timeIndex["month.end"] as? Int,
+                                            maxValue: 12,
+                                            startValue: 1 // months are 1-indexed
+                                       )
+            timeIndex["day.range"] = getRange(
+                                            start: timeIndex["day.start"] as? Int,
+                                            end: timeIndex["day.end"] as? Int,
+                                            maxValue: daysInMonth(month: startComponents.month ?? 0, year: startComponents.year ?? 0),
+                                            startValue: 1 // days are 1-indexed
+                                     )
+            timeIndex["hour.range"] = getRange(
+                                            start: timeIndex["hour.start"] as? Int,
+                                            end: timeIndex["hour.end"] as? Int,
+                                            maxValue: 23
+                                      )
+            timeIndex["minute.range"] = getRange(
+                                            start: timeIndex["minute.start"] as? Int,
+                                            end: timeIndex["minute.end"] as? Int,
+                                            maxValue: 59
+                                        )
+            timeIndex["dayMinute.range"] = getRange(
+                                            start: timeIndex["dayMinute.start"] as? Int,
+                                            end: timeIndex["dayMinute.end"] as? Int,
+                                            maxValue: 1439
+                                        )
+            timeIndex["15minBucket.range"] = getRange(
+                                            start: timeIndex["15minBucket.start"] as? Int,
+                                            end: timeIndex["15minBucket.end"] as? Int,
+                                            maxValue: 95
+                                        )
+            timeIndex["second.range"] = getRange(start: timeIndex["second.start"] as? Int, end: timeIndex["second.end"] as? Int, maxValue: 59)
+        }
+        
+        return timeIndex
+    }
+    
     /// Adds a new `HKSample` to the Firestore.
     /// - Parameter response: The `HKSample` that should be added.
     func add(sample: HKSample) async {
@@ -84,9 +192,9 @@ extension PrismaStandard {
         
         
         // convert the startDate of the HKSample to local time
-        let startDatetime = sample.startDate
-        let effectiveTimestamp = startDatetime.localISOFormat()
-        let endDatetime = sample.endDate.localISOFormat()
+        let timeIndex = getTimeIndex(sample: sample)
+        let effectiveTimestamp = sample.startDate.localISOFormat()
+        
         
         let path: String
         // path = HEALTH_KIT_PATH/raw/YYYY-MM-DDThh:mm:ss.mss
@@ -112,6 +220,7 @@ extension PrismaStandard {
             let encoder = FirebaseFirestore.Firestore.Encoder()
             var firestoreResource = try encoder.encode(resource)
             firestoreResource["device"] = deviceName
+            firestoreResource["time"] = timeIndex
             try await Firestore.firestore().document(path).setData(firestoreResource)
         } catch {
             print("Failed to set data in Firestore: \(error.localizedDescription)")
