542d1e84236519fd45b5aa06ac009fe5269057d5
Evelyn <122575337+evelyn-hur@users.noreply.github.com>
2024-02-28 22:52:12 -0800

    First attempt at toggle mapping identifier string to bool
    Created function createTogglesMapping in Privacy Controls module. Previously, sample types (e.g, "step count") were hardcoded to toggle bool. Currently trying to use getSampleIdentifier to map identifier string to toggle bool. We have a bug where we can't access prisma delegate healthKit since it's private.

--- Prisma/PrivacyControls/PrivacyControls.swift
+++ Prisma/PrivacyControls/PrivacyControls.swift
@@ -3,6 +3,7 @@
 //  Prisma
 //
 //  Created by Dhruv Naik on 2/1/24.
+// Edited by Evelyn Hur and Caroline on 2/28/24.
 
 //  SPDX-FileCopyrightText: 2023 Stanford University
 //
@@ -14,26 +15,53 @@ import SwiftUI
 
 
 @Observable
-public class PrivacyModule: DefaultInitializable, EnvironmentAccessible {
+public class PrivacyModule: Module, DefaultInitializable, EnvironmentAccessible {
     var configuration: Configuration {
         Configuration(standard: PrismaStandard()) { }
     }
     
     public required init() {}
     
-    public func getCurrentToggles() -> [String: Bool] {
-        [
-            "includeStepCountUpload": includeStepCountUpload,
-            "includeActiveEnergyBurned": includeActiveEnergyBurned,
-            "includeDistanceWalkingRunning": includeDistanceWalkingRunning,
-            "includeVo2Max": includeVo2Max,
-            "includeHeartRate": includeHeartRate,
-            "includeRestingHeartRate": includeRestingHeartRate,
-            "includeOxygenSaturation": includeOxygenSaturation,
-            "includeRespiratoryRate": includeRespiratoryRate,
-            "includeWalkingHRAverage": includeWalkingHRAverage
-        ]
+    @StandardActor var standard: PrismaStandard
+    let prismaDelegate = PrismaDelegate()
+    
+    // how to access healthKit samples from Prisma Delegate if private
+    public func createTogglesMapping() -> [String: Bool] {
+        var togglesMapping: [String: Bool] = [:]
+        let samples = prismaDelegate.healthKit
+        for sample in samples {
+            let identifier = standard.getSampleIdentifier(sample)
+            togglesMapping[identifier] = true
+        }
+        return togglesMapping
     }
+    
+//    var sampleToToggleNameMapping: [String: Bool] = [
+//        getSampleIdentifier(HKSample): true,
+//        HKQuantityType.quantityType(forIdentifier: .stepCount): "includeStepCountUpload",
+//        HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning): "includeDistanceWalkingRunning",
+//        HKQuantityType.quantityType(forIdentifier: .vo2Max): "includeVo2Max",
+//        HKQuantityType.quantityType(forIdentifier: .heartRate): "includeHeartRate",
+//        HKQuantityType.quantityType(forIdentifier: .restingHeartRate): "includeRestingHeartRate",
+//        HKQuantityType.quantityType(forIdentifier: .oxygenSaturation): "includeOxygenSaturation",
+//        HKQuantityType.quantityType(forIdentifier: .respiratoryRate): "includeRespiratoryRate",
+//        HKQuantityType.quantityType(forIdentifier: .walkingHeartRateAverage): "includeWalkingHeartRateAverage"
+//    ]
+        
+    
+//    public func getCurrentToggles() -> [String: Bool] {
+//        [
+//            "includeStepCountUpload": includeStepCountUpload,
+//            "includeActiveEnergyBurned": includeActiveEnergyBurned,
+//            "includeDistanceWalkingRunning": includeDistanceWalkingRunning,
+//            "includeVo2Max": includeVo2Max,
+//            "includeHeartRate": includeHeartRate,
+//            "includeRestingHeartRate": includeRestingHeartRate,
+//            "includeOxygenSaturation": includeOxygenSaturation,
+//            "includeRespiratoryRate": includeRespiratoryRate,
+//            "includeWalkingHRAverage": includeWalkingHRAverage
+//        ]
+//}
 //    public func getLastTimestamps(quantityType: String) async -> [String] {
 //        var path: String = ""
 //
