a214a6a17fa7ab660de1ef425d4af31f0edac17d
EvelynBunnyDev <evsong@stanford.edu>
2024-03-03 23:55:55 -0800

    Update ChatView.swift

--- Prisma/Chat/ChatView.swift
+++ Prisma/Chat/ChatView.swift
@@ -34,8 +34,6 @@ struct ChatView: View {
                         Text("Invalid URL")
                     }
                 } else {
-                    // Handle case where token is nil
-                    // Text("Failed to get JWT token")
                     ProgressView()
                 }
             }
@@ -54,6 +52,10 @@ struct ChatView: View {
                 } catch {
                     print("Firebase Auth failed \(error)")
                 }
+                guard await ((try? self.signInWithFirebase()) != nil) else {
+                    print("Firebase Auth failed")
+                    return
+                }
             }
         }
     }
@@ -66,48 +68,6 @@ struct ChatView: View {
 extension ChatView {
     func signInWithFirebase() async throws {
         token = try await Auth.auth().currentUser?.getIDToken()
-    }
-    
-    func generateJWT(completion: @escaping (String?) -> Void ) {
-        if let currentUser = Auth.auth().currentUser {
-            // Generating JWT Token
-            currentUser.getIDTokenForcingRefresh(true) { (token, error) in
-                if let error = error {
-                    print("Error getting ID token: \(error.localizedDescription)")
-                    completion(nil)
-                } else if let token = token { // Setting the JWT token and send it to chat
-                    print("JWT is: \(token)")
-                    sendTokenToBackend(token: token, completion: completion)
-                } else {
-                    print("No token received.")
-                    completion(nil)
-                }
-            }
-        } else {
-            print("No current user")
-            completion(nil)
-        }
-    }
-    
-    func sendTokenToBackend(token: String, completion: @escaping (String?) -> Void) {
-        let url = URL(string: "http://localhost:5000")! // Replace with actual website, once created
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            // Handle response from backend
-            if let data = data, let responseString = String(data: data, encoding: .utf8) {
-                print("Response from backend: \(responseString)")
-                completion(responseString)
-            } else if let error = error {
-                print("Error sending token to backend: \(error.localizedDescription)")
-                completion(nil)
-            } else {
-                print("No response received from backend")
-                completion(nil)
-            }
-        }
-        task.resume()
     }
 }
 
