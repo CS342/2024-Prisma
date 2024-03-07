927c6e35cb725b1f508fed92c588eceddfc23c7a
Caroline Tran <89322465+carolinentran@users.noreply.github.com>
2024-03-07 00:42:28 -0800

    ManageDataView dependent on PrismaDelegate sample types
    Previously, the ManageDataView had a list created from a hardcoded dictionary (which would have shown more/fewer sample types than what was defined in the PrismaDelegate). Modified all other views dependent on PrivacyModule.
    
    Restructured the communication between PrivacyModule and ManageDataView. Originally, ManageDataView was passed a list of structs with all the icon, identifier, UI string, and toggle status elements. This is repetitive with the dictionary that is already in PrivacyModule so I just passed the vector of sorted identifier strings instead (and ManageDataView and DeleteDataView accessed necessary information accordingly)

--- Prisma/PrivacyControls/ManageDataView.swift
+++ Prisma/PrivacyControls/ManageDataView.swift
@@ -16,23 +16,32 @@
 import SwiftUI
 
 struct ManageDataView: View {
-    @Environment(PrivacyModule.self) private var privacyModule
+//    @Environment(PrivacyModule.self) private var privacyModule
+    @EnvironmentObject var privacyModule: PrivacyModule
+
     var body: some View {
         NavigationView {
-            List(privacyModule.dataCategoryItems, id: \.name) { item in
-                NavigationLink(destination: DeleteDataView(categoryIdentifier: item.name)) {
-                    HStack(alignment: .center, spacing: 10) {
-                        Image(systemName: item.iconName)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 35, height: 35)
-                            .accessibility(label: Text("accessibility text temp"))
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text(privacyModule.identifierUIString[item.name] ?? "Identifier UI String Not Found")
-                                .font(.headline)
-                            Text(item.enabledStatus)
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
+            List {
+                ForEach(privacyModule.sortedSampleIdentifiers, id: \.self) { sampleIdentifier in
+                    NavigationLink(
+                        destination: DeleteDataView(
+                            categoryIdentifier: privacyModule.identifierInfo[sampleIdentifier]?.identifier ?? "missing identifier string"
+                        )
+                    ) {
+                        HStack(alignment: .center, spacing: 10) {
+                            Image(systemName: privacyModule.identifierInfo[sampleIdentifier]?.iconName ?? "missing icon name")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 35, height: 35)
+                                .accessibility(label: Text("accessibility text temp"))
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(privacyModule.identifierInfo[sampleIdentifier]?.uiString ?? "missing ui identifier string")
+                                    .font(.headline)
+                                // assume a default value of false if there is a nil value in enabledBool
+                                Text((privacyModule.identifierInfo[sampleIdentifier]?.enabledBool ?? false) ? "Enabled" : "Disabled")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
                         }
                     }
                 }
@@ -45,3 +54,24 @@ struct ManageDataView: View {
 #Preview {
     ManageDataView()
 }
+
+//
+//
+// List(privacyModule.sortedSampleTypesList) { item in
+//    NavigationLink(destination: DeleteDataView(categoryIdentifier: item.identifier)) {
+//        HStack(alignment: .center, spacing: 10) {
+//            Image(systemName: item.iconName)
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 35, height: 35)
+//                .accessibility(label: Text("accessibility text temp"))
+//            VStack(alignment: .leading, spacing: 4) {
+//                Text(privacyModule.identifierUIString[item.name] ?? "Identifier UI String Not Found")
+//                    .font(.headline)
+//                Text(item.enabledStatus)
+//                    .font(.subheadline)
+//                    .foregroundColor(.gray)
+//            }
+//        }
+//    }
+// }
